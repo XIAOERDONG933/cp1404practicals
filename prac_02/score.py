05e00f85de770bcefb1616f90d2f264b3a53a319
27144 <xedget111@outlook.com>
2025-02-06 20:21:20 +0800

    Refactor score.py and determine a random score

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,12 +1,22 @@
+import random
+
 def main():
     score = float(input("Enter score: "))
+    print(determine_score(score))
+    random_score = random.randint(0, 100) # generate a random score ranging from 0 to 100
+    print(random_score)
+    print(determine_score(random_score))
+
+def determine_score(score):
+    """Determine score status"""
     if score < 0 or score > 100:
-        print("Invalid score")
+        return "Invalid score"
     elif score >= 90:
-        print("Excellent")
+        return "Excellent"
     elif score >= 50:
-        print("Passable")
+        return "Passable"
     else:
-        print("Bad")
+        return "Bad"
+
 
 main()
