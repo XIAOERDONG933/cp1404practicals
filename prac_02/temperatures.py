ebc7c10c3b16d653be188ec4e38103823ccda401
27144 <xedget111@outlook.com>
2025-02-06 19:57:59 +0800

    Refactor temperatures.py

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -1,27 +1,34 @@
-def celsius_to_fahrenheit(celsius):
-    return celsius * 9.0 / 5 + 32
-
-def fahrenheit_to_celsius(fahrenheit):
-    return 5 / 9 * (fahrenheit - 32)
-
 MENU = """C - Convert Celsius to Fahrenheit
 F - Convert Fahrenheit to Celsius
 Q - Quit"""
-print(MENU)
-choice = input(">>> ").upper()
 
-while choice != "Q":
-    if choice == "C":
-        celsius = float(input("Celsius: "))
-        fahrenheit = celsius_to_fahrenheit(celsius)
-        print(f"Result: {fahrenheit:.2f} F")
-    elif choice == "F":
-        fahrenheit = float(input("Fahrenheit: "))
-        celsius = fahrenheit_to_celsius(fahrenheit)
-        print(f"Result: {celsius:.2f} C")
-    else:
-        print("Invalid option")
+def main():
     print(MENU)
     choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "C":
+            celsius = float(input("Celsius: "))
+            fahrenheit = convert_celsius_to_fahrenheit(celsius)
+            print(f"Result: {fahrenheit:.2f} F")
+        elif choice == "F":
+            fahrenheit = float(input("Fahrenheit: "))
+            celsius = convert_fahrenheit_to_celsius(fahrenheit)
+            print(f"Result: {celsius:.2f} C")
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
 
-print("Thank you.")
+def convert_celsius_to_fahrenheit(celsius):
+    """convert_celsius_to_fahrenheit"""
+    return celsius * 9.0 / 5 + 32
+
+
+def convert_fahrenheit_to_celsius(fahrenheit):
+    """convert_fahrenheit_to_celsius"""
+    return 5 / 9 * (fahrenheit - 32)
+
+
+
+main()
