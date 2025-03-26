fbb5703384ef21da577586d8cfdc02690f60bf9b
27144 <xedget111@outlook.com>
2025-03-26 20:56:44 +0800

    Finish Mile to Kilometres Converter exercise stage 2

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -13,12 +13,22 @@ class ConvertMilesToKM(App):
         return self.root
 
     def handle_convert(self,input_mile):
-        result = float(input_mile) * KM_PER_MILE
+        input_mile = self.get_valid_input_mile(input_mile)
+        result = input_mile * KM_PER_MILE
         self.output_message = str(result)
 
     def handle_increment(self,input_mile, increment):
-        updated_mile = float(input_mile) + increment
+        input_mile = self.get_valid_input_mile(input_mile)
+        updated_mile = input_mile + increment
         self.root.ids.input_mile.text = str(updated_mile)
         self.handle_convert(updated_mile)
+
+    @staticmethod
+    def get_valid_input_mile(input_mile):
+        try:
+            input_mile = float(input_mile)
+        except ValueError:
+            input_mile = 0
+        return input_mile
 
 ConvertMilesToKM().run()
