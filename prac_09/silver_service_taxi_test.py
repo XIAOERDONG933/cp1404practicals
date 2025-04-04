3ae163ceff8e607d1aa77d818642d3db30aace22
27144 <xedget111@outlook.com>
2025-04-04 15:58:32 +0800

    Enhance SilverServiceTaxi exercise

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -3,12 +3,12 @@ from silver_service_taxi import SilverServiceTaxi
 def main():
     sst = SilverServiceTaxi("Toyota", 100, 2)
     sst.drive(18)
-    assert sst.get_fare() == 48.78, "The fare should be 48.78"
-    print(f"You got it! Current fare is {sst.get_fare()}")
+    assert sst.get_fare() == 48.80, "The fare should be 48.80"
+    print(f"You got it! Current fare is {sst.get_fare():.2f}")
 
     benz = SilverServiceTaxi("Benz", 300, 5)
-    benz.drive(20)
-    assert benz.get_fare() == 127.5, "The fare should be 127.5"
-    print(f"You got it! Current fare is {benz.get_fare()}")
+    benz.drive(17)
+    assert benz.get_fare() == 109.10, "The fare should be 109.10"
+    print(f"You got it! Current fare is {benz.get_fare():.2f}")
 
 main()
