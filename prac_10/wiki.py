1abd10f744a7d8f87f7587cd7f0b84d8e067161d
27144 <xedget111@outlook.com>
2025-04-11 21:18:21 +0800

    Complete wikipedia exercise

---
+++ prac_10/wiki.py
@@ -0,0 +1,24 @@
+import wikipedia
+from wikipedia.exceptions import DisambiguationError, PageError
+
+
+def main():
+    title = input("Enter page title: ").strip()
+    while title != "":
+        try:
+            page = wikipedia.page(title)
+            print(page.title)
+            print(wikipedia.summary(page.title))
+            print(page.url)
+        except DisambiguationError:
+            print("We need a more specific title. Try one of the following, or a new search:")
+            suggestions =  wikipedia.search(title)
+            print(suggestions)
+        except PageError:
+        print(f"Page id \"{title}\" does not match any pages. Try another id!")
+        print()
+        title = input("Enter page title: ").strip()
+    print("Thank you.")
+
+
+main()
