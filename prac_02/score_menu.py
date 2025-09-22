e343b4a3b91fc1edc603e2e8fda9958209587edd
27144 <xedget111@outlook.com>
2025-09-22 14:12:58 +0800

    Complete score_menu.py exercise

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -1,50 +1,53 @@
-"""
-Use menu to ask the user for a score and print the score status
-and stars
-"""
-MENU = """G - Get a valid score (must be 0-100 inclusive)
-P - Print result
-S - Show stars
-Q - Quit
-"""
+"""This module implements a score menu program that allows users to obtain valid scores,
+print evaluation results, and display as many stars as their scores."""
+
+MENU = """(G)et a valid score
+(P)rint result
+(S)how stars
+(Q)uit"""
 
 def main():
-    print(MENU)
-    # Get a valid score
+    """Run the score menu program and continue until the user exits."""
     score = get_valid_score()
-    choice = input(">>> ").upper()
+    print(MENU)
+    choice = input("Enter choice: ").upper()
     while choice != "Q":
         if choice == "G":
             score = get_valid_score()
         elif choice == "P":
-            print(determine_score(score))
+            result = get_result(score)
+            print(f"Your final grade is: {result}")
         elif choice == "S":
-            print(score * "*")
+            show_stars(score)
+        elif choice == "Q":
+            print("Farewell!")
         else:
-            print("Invalid option")
-        print(MENU)
-        choice = input(">>> ").upper()
-    print("Goodbye")
+            print("Invalid option, please try again.")
 
+        print(MENU)
+        choice = input("Enter choice: ").upper()
 
 def get_valid_score():
-    "Get a valid score"
-    score = int(input("Enter score: "))
+    """Obtain a valid score."""
+    score = float(input("Enter a valid score (0-100): "))
     while score < 0 or score > 100:
-        print("Invalid score")
-        score = int(input("Enter score: "))
+        print("Invalid score. Score must be between 0 and 100.")
+        score = float(input("Enter a valid score (0-100): "))
     return score
 
-
-def determine_score(score):
-    """Determine score status"""
+def get_result(score):
+    """Determine the final grade based on the given score."""
     if score < 0 or score > 100:
         return "Invalid score"
-    elif score >= 90:
+    elif score > 90:
         return "Excellent"
-    elif score >= 50:
+    elif score > 50:
         return "Passable"
     else:
         return "Bad"
+
+def show_stars(score):
+    """Display asterisks with the same number of integer parts as the fraction."""
+    print("*" * int(score))
 
 main()
