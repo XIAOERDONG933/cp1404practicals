756efce5e3d7911660eeedeba1b5a39414609c28
27144 <xedget111@outlook.com>
2025-10-01 21:33:45 +0800

    Complete files.py exercise and test

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,46 +1,38 @@
-NAME_FILE = "name.txt"
-"""
-1. Write code that asks the user for their name, then opens a file called name.txt
-and writes that name to it. Use and for this question.
-"""
-name = input("Enter your name: ")
-out_file = open(NAME_FILE, "w")
-print(name, file=out_file)
-out_file.close()
 
 """
-2. In the same file, but as if it were a separate program, write code that 
-opens "name.txt" and reads the name (as above) then prints (note the exact output),
+1. Write code that asks the user for their name, then opens a file called name.txt and writes that name to it.
+Use open and close for this question.
+"""
+name = input("Please enter your name: ")
+file = open("name.txt", "w")
+file.write(name)
+file.close()
+
+"""
+2. In the same file, but as if it were a separate program, write code that opens "name.txt" and reads the name (as above) then prints (note the exact output),
 Hi Bob! (or whatever the name is in the file). Do not simply print the user's input variable!
-Use and for this question.
+Use open and close for this question.
 """
-in_file = open(NAME_FILE, "r")
-name = in_file.read().strip("\n")
+file = open("name.txt", "r")
+name = file.read().strip()
+file.close()
 print(f"Hi {name}!")
-in_file.close()
-
-
-NUMBER_FILENAME = "numbers.txt"
-"""
-3. Write code that opens number.txt , reads only the first two numbers, 
-adds them together then prints the result, which should be... 59. 
-Use instead of and for this question.
-"""
-with open(NUMBER_FILENAME, "r") as in_file:
-    num1 = int(in_file.readline().strip("\n")) # the number on line 1
-    num2 = int(in_file.readline().strip("\n")) # the number on line 2
-    print(num1 + num2)
-
 
 """
-4. Now write a fourth block of code that prints the total for all lines in number.txt. 
-This should work for a file with any number of numbers. Use instead of open
-and close for this question.
+3. Create a text file called numbers.txt and save it in your prac directory. Put the following three numbers on separate lines in the file and save it:
+17
+42
+400
+Write code that opens numbers.txt, reads only the first two numbers, adds them together then prints the result, which should be... 59. Use with instead of open and close for this question.
 """
+with open("numbers.txt", "r") as file:
+    first_number = int(file.readline().strip())
+    second_number = int(file.readline().strip())
+print(first_number + second_number)
 
-with open(NUMBER_FILENAME, "r") as in_file:
-    total = 0
-    for line in in_file:
-        num = int(line.strip("\n"))
-        total += num
-    print(total)
+"""
+4. Now write a fourth block of code that prints the total for all lines in numbers.txt. This should work for a file with any number of numbers. Use with instead of open and close for this question.
+"""
+with open("numbers.txt", "r") as file:
+    total = sum(int(line.strip()) for line in file)
+print(total)
