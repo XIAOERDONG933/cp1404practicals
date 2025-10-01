a2ed2cbd5f42a827402832f4b1a9e8f77080b6ff
27144 <xedget111@outlook.com>
2025-10-01 21:26:16 +0800

    Complete exceptions_demo.py exercise

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -2,25 +2,30 @@
 CP1404/CP5632 - Practical
 Answer the following questions:
 1. When will a ValueError occur?
-When either numberator or denominator is not an integer value.
+When the user inputs a numerator or denominator that is not a valid integer.
 
 2. When will a ZeroDivisionError occur?
-When the entered denominator is 0.
+When the denominator entered by the user is 0, it causes the calculation to be divided by zero.
 
 3. Could you change the code to avoid the possibility of a ZeroDivisionError?
-Just pre-check the value of denominator, make sure it is not zero before the division.
+Yes, and I have made the following changes to the code
 """
 
-try:
-    numerator = int(input("Enter the numerator: "))
-    denominator = int(input("Enter the denominator: "))
-    if denominator == 0:
-        print("The denominator cannot be 0")
-    else:
-        fraction = numerator / denominator
-        print(fraction)
-except ValueError:
-    print("Numerator and denominator must be valid numbers!")
-except ZeroDivisionError:
-    print("Cannot divide by zero!")
+valid_input = False
+
+while not valid_input:
+    try:
+        numerator = int(input("Enter the numerator: "))
+        denominator = int(input("Enter the denominator: "))
+
+        if denominator != 0:
+            fraction = numerator / denominator
+            print(fraction)
+            valid_input = True
+        else:
+            print("Cannot divide by zero! Please enter new denominator.")
+
+    except ValueError:
+        print("Numerator and denominator must be valid numbers!")
+
 print("Finished.")
