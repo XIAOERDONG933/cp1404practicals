d2d91823d411bd06715d2499b041eeedc970bda8
27144 <xedget111@outlook.com>
2025-10-01 21:23:17 +0800

    Complete randoms.py exercise

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -1,16 +1,27 @@
 import random
 
 print(random.randint(5, 20))  # line 1
-# line 1:output a random integer between 5(inclusive) and 20(inclusive).
-# The smallest possible output is 5, and the largest possible output is 20.
-
 print(random.randrange(3, 10, 2))  # line 2
-# line 2: ouput a random nubmer in [3, 5, 7, 9]
-# The smallest possible output is 3, and the largest possible output is 9.
-# Not possbile to output number 4.
-
 print(random.uniform(2.5, 5.5))  # line 3
-# line 3: output a random number from 2.5(inclusive) to 5.5(inclusive).
-# The smallest possible output is 2.5, and the largest possible output is 5.5.
 
-print(random.uniform(1, 100)) # produce a random number between 1 and 100 inclusive.
+# What did you see on line 1?
+# This program uses the randint() function of the random module.
+# This will generate a random integer between 5 and 20.
+# What was the smallest number you could have seen, what was the largest?
+# The smallest number is 5, and the largest number is 20
+
+# What did you see on line 2?
+# This program uses the randrange() function of the random module.
+# This will generate a random integer within the range of [3,5,7,9].
+# What was the smallest number you could have seen, what was the largest?
+# The smallest number is 3, and the largest number is 9
+# Could line 2 have produced a 4?
+# No, because the step is 2.
+
+# What did you see on line 3?
+# This program uses the uniform() function of the random module. This produces a random float between 2.5 and 5.5.
+# What was the smallest number you could have seen, what was the largest?
+# The smallest number is 2.5, and the largest number is 5.5
+
+# Write code, not a comment, to produce a random number between 1 and 100 inclusive.
+print(random.randint(1, 100))
