202d2907b45179b71d9eba36b73b425effbdae21
27144 <xedget111@outlook.com>
2025-10-30 19:31:12 +0800

    Add some docstring

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -1,17 +1,1 @@
-from programming_language import ProgrammingLanguage
-
-def main():
-    """Test code to show how to use ProgrammingLanguage."""
-    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
-    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
-    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
-    print(python)
-    print(ruby)
-    print(visual_basic)
-    programming_languages = [python,ruby,visual_basic]
-    print("The dynamically typed languages are:")
-    for programming_language in programming_languages:
-        if programming_language.is_dynamic():
-            print(programming_language.name)
-
-main()
+"""CP1404/CP5632 Practical - Test ProgrammingLanguage class."""
