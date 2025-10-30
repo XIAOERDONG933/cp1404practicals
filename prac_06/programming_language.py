0a78618f9d0aa37f0a8934530d9ec70890cf52bd
27144 <xedget111@outlook.com>
2025-10-30 19:29:11 +0800

    Change some comments

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -14,3 +14,7 @@ class ProgrammingLanguage:
         self.typing = typing
         self.reflection = reflection
         self.year = year
+
+    def __str__(self):
+        """Return the formatted string representation of the programming language."""
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
