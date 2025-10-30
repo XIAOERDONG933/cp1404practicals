e89197defd45819906f953381fba15fb46869610
27144 <xedget111@outlook.com>
2025-10-30 19:28:41 +0800

    Complete the coding of the ProgramingLanguage class

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -4,3 +4,13 @@ Programming Languages
 Estimate: 20 minutes
 Actual:   10 minutes
 """
+
+class ProgrammingLanguage:
+    """Represent a programming language object."""
+
+    def __init__(self, name, typing, reflection, year):
+        """Initialize a programming language instance."""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
