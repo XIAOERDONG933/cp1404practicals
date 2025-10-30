61d16c6fccc77b99079eb6562f70810de157d552
27144 <xedget111@outlook.com>
2025-10-30 19:40:22 +0800

    Add some docstring and complete time prediction

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,44 +1,6 @@
+"""CP1404/CP5632 Practical - Store all users' guitars and print them."""
 """
 Guitars
-Estimate: 10 minutes
-Actual:   20 minutes
+Estimate: 20 minutes
+Actual:   21 minutes
 """
-from guitar import Guitar
-
-def main():
-    print("My guitars!")
-    guitars = []
-    guitar = get_guitar_info()
-    while guitar is not None:
-        guitars.append(guitar)
-        display_guitar_info(guitar)
-        guitar = get_guitar_info()
-    display_total_guitars(guitars)
-
-
-def get_guitar_info():
-    """Get guitar info"""
-    name = input("Name: ")
-    if name == "":
-        return None
-    year = int(input("Year: "))
-    cost = float(input("Cost: $"))
-    new_guitar = Guitar(name, year, cost)
-    return new_guitar
-
-
-def display_guitar_info(guitar):
-    """Display guitar info"""
-    print(f"{guitar.name} ({guitar.year}): ${guitar.cost:.2f} added.\n")
-
-
-def display_total_guitars(guitars):
-    """Display total guitars"""
-    print("There are my guitars: ")
-    for i, guitar in enumerate(guitars, 1):
-        vintage_string = " (vintage)" if guitar.is_vintage() else ""
-        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), "
-              f"worth ${guitar.cost:10,.2f}{vintage_string}")
-
-
-main()
