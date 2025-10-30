fe1778b3348517fd222fe15970c667b9eb19640f
27144 <xedget111@outlook.com>
2025-10-30 19:35:58 +0800

    Complete the coding of the guitar class

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -10,3 +10,9 @@ CURRENT_YEAR = 2025
 class Guitar:
     """Represent a Guitar object."""
     VINTAGE_AGE = 50
+
+    def __init__(self, name="", year=0, cost=0):
+        """Initialise a Car Guitar."""
+        self.name = name
+        self.year = year
+        self.cost = cost
