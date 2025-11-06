c441057b5b06d79f75f6cdddcb8140daf5a2868e
27144 <xedget111@outlook.com>
2025-11-06 20:52:25 +0800

    The function of adding input content

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -5,3 +5,23 @@ My Guitars
 from prac_07.guitar import Guitar, CURRENT_YEAR
 
 FILENAME = "guitars.csv"
+
+def main():
+    """Main program to manage guitar collection."""
+    guitars = load_guitars(FILENAME)
+
+    print("\nOriginal guitar list:")
+    display_guitars(guitars)
+
+    guitars.sort()
+    print("\nSorted by year (oldest to newest):")
+    display_guitars(guitars)
+
+    print("\nEnter new guitars (blank name to finish)")
+    guitars = add_guitars_recursively(guitars)
+
+    save_guitars(FILENAME, guitars)
+
+    print("\nFinal guitar list:")
+    guitars.sort()
+    display_guitars(guitars)
