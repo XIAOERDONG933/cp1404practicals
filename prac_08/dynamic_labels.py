41d9fba0f9abe302c4330908ec7499650aa5245e
27144 <xedget111@outlook.com>
2025-11-13 20:20:34 +0800

    Introduce the content as per the requirements.

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -1,20 +1,7 @@
+"""
+CP1404/CP5632 Practical
+Kivy GUI program about Dynamic Labels
+"""
 from kivy.app import App
-from kivy.lang import Builder
 from kivy.uix.label import Label
-
-class DynamicLabelsApp(App):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.names = ["Alice", "Bob", "Charlie", "David"]
-
-    def build(self):
-        self.root = Builder.load_file('dynamic_labels.kv')
-        for name in self.names:
-            temp_label = Label(text=name)
-            self.root.ids.main.add_widget(temp_label)
-        return self.root
-
-if __name__ == '__main__':
-    DynamicLabelsApp().run()
-
-
+from kivy.lang import Builder
