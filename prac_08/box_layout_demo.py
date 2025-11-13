4fc44735455cf51cc2859eb4d494b56303ebfd89
27144 <xedget111@outlook.com>
2025-11-13 20:12:31 +0800

    Complete the preparatory work for code writing

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -1,18 +1,6 @@
+"""
+CP1404/CP5632 Practical
+Kivy GUI program about BoxLayoutDemo
+"""
 from kivy.app import App
 from kivy.lang import Builder
-
-
-class BoxLayoutDemo(App):
-    def build(self):
-        self.title = "Box Layout Demo"
-        self.root = Builder.load_file('box_layout.kv')
-        return self.root
-
-    def handle_greet(self):
-        self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
-
-    def handle_clear(self):
-        self.root.ids.output_label.text = ""
-        self.root.ids.input_name.text = ""
-
-BoxLayoutDemo().run()
