fe942eb2ce9638361078980727d7d2ec3331c43e
27144 <xedget111@outlook.com>
2025-11-20 19:53:22 +0800

    Complete the preparatory work for code writing

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -1,16 +1,5 @@
-from taxi import Taxi
-
-class SilverServiceTaxi(Taxi):
-    """Represents a silver service taxi"""
-    flagfall = 4.50
-
-    def __init__(self, name, fuel, fanciness):
-        super().__init__(name, fuel)
-        self.fanciness = fanciness
-        self.price_per_km = Taxi.price_per_km * fanciness
-
-    def __str__(self):
-        return f"{super().__str__()} plus flagfall of {self.flagfall}"
-
-    def get_fare(self):
-        return super().get_fare() + self.flagfall
+"""
+CP1404/CP5632 Practical
+silver service taxi
+"""
+from prac_09.taxi import Taxi
