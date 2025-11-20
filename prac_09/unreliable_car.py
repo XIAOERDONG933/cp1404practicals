0038aef4f7b2e1890d8849f3a73b26e4de03b998
27144 <xedget111@outlook.com>
2025-11-20 19:49:04 +0800

    The necessary preparatory work required for completing the code writing

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -1,17 +1,6 @@
+"""
+CP1404/CP5632 Practical
+unreliable car
+"""
 from prac_09.car import Car
 import random
-
-
-class UnreliableCar(Car):
-    """Represents an unreliable car."""
-
-    def __init__(self, name, fuel, reliability):
-        super().__init__(name, fuel)
-        self.reliability = reliability
-
-    def drive(self, distance):
-        """Drives the car by the given distance."""
-        random_num = random.uniform(0, 100)
-        if random_num < self.reliability:
-            return super().drive(distance)
-        return 0
