2c9e738bcdc3e44c7642ff2ced1b5d6069b1d9c4
27144 <xedget111@outlook.com>
2025-11-20 19:45:28 +0800

    Prepare the necessary prerequisites for writing the code

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -1,15 +1,5 @@
-from prac_09.taxi import Taxi
-
-my_taxi = Taxi("Prius 1", 100)
-
-my_taxi.drive(40)
-
-print(my_taxi)
-
-print(f"Current fare is {my_taxi.get_fare()}")
-
-my_taxi.start_fare()
-my_taxi.drive(100)
-
-print(my_taxi)
-print(f"Current fare is {my_taxi.get_fare()}")
+"""
+CP1404/CP5632 Practical
+taxi test
+"""
+from taxi import Taxi
